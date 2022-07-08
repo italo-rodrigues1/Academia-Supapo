9c2fc47339b9ec31ae47552f65c672a63c5f2782
italo-rodrigues1 <italosport110@hotmail.com>
2022-07-08 00:33:41 -0300

    Organized project

---
+++ src/pages/start/index.jsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Start(){
+    return (
+        <div>
+            <Image src="/assets/StartImg.svg" alt="Start logo" width="100%" height="100%"/>
+        </div>
+    )
+}
